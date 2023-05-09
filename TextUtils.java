effd6ab9ed27934128d3cb24d9d8e27b14c9cc8d
lucaonnis00 <119352292+lucaonnis00@users.noreply.github.com>
2023-05-09 12:40:40 +0200

    Add files via upload

---
+++ TextUtils.java
@@ -0,0 +1,24 @@
+package com.onnis.utils;
+
+public class TextUtils {
+	public static String[] longestWords(String text) {
+	    String[] sentences = text.split("(?<!\\d)\\.(?!\\d)|\\?|!");
+	    String[] longestWords = new String[sentences.length];
+	    for (int i = 0; i < sentences.length; i++) {
+	        longestWords[i] = longestWord(sentences[i]) + " (frase " + (i + 1) + ")";
+	    }
+	    return longestWords;
+	}
+
+    public static String longestWord(String text) {
+        String[] words = text.split("[\\p{Punct}&&[^.@]]+|\\s+");
+        String longestWord = "";
+        for (String word : words) {
+            if (word.length() > longestWord.length()) {
+                longestWord = word;
+            }
+        }
+        return longestWord;
+    }
+}
+
