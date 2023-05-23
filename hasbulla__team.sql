d5a88fbdbee58151441d52fa287f9ff9b9f23284
danielpaliuc <119485982+danielpaliuc@users.noreply.github.com>
2023-05-23 12:06:38 +0200

    Add files via upload

---
+++ hasbulla__team.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mag 23, 2023 alle 12:05
+-- Versione del server: 10.4.25-MariaDB
+-- Versione PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hasbulla _team`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `testi`
+--
+
+CREATE TABLE `testi` (
+  `id_testo` int(11) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `contenuto` text DEFAULT NULL,
+  `tot_parole` int(11) DEFAULT NULL,
+  `tot_frasi` int(11) DEFAULT NULL,
+  `parola_piu_usata` varchar(255) DEFAULT NULL,
+  `parola_piu_lunga` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `nparole_per_frase` int(11) DEFAULT NULL,
+  `parola_piu_usata_frase` varchar(255) DEFAULT NULL,
+  `parola_piu_lunga_frase` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `testi`
+--
+ALTER TABLE `testi`
+  ADD PRIMARY KEY (`id_testo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
