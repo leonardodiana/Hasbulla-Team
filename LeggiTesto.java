ef91afc8487dfbde5774d41da83b3d1779eef8e5
lucaonnis00 <119352292+lucaonnis00@users.noreply.github.com>
2023-05-30 13:02:15 +0200

    Update LeggiTesto.java

--- LeggiTesto.java
+++ LeggiTesto.java
@@ -11,14 +11,25 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import com.onnis.entities.Frase;
+import com.onnis.entities.Parole;
 import com.onnis.entities.Text;
+import com.onnis.repos.FraseDAO;
+import com.onnis.repos.ParolaDAO;
 import com.onnis.repos.TestoDAO;
+import com.onnis.repos.TestoFraseDAO;
 
 @Component
 public class LeggiTesto {
 	
 	@Autowired
 	private TestoDAO dao;
+	@Autowired
+	private FraseDAO daoFrase;
+	@Autowired
+	private TestoFraseDAO daoTF;
+	@Autowired
+	private ParolaDAO daoParola;
 
 	@EventListener(ApplicationReadyEvent.class)
 	public void leggiFile() {
@@ -45,11 +56,33 @@ public class LeggiTesto {
 	String text = sb.toString();
 	
 	
-	Text textEntity = new Text();
+	Text textEntity = new Text();//dare id
 	textEntity.setContenuto(text);
+	textEntity.setNumeroParole(daoParola.count());
 	dao.save(textEntity);
+	
+	String[] frasi = text.split("\\.\\s+");
+	for (String frase : frasi) {
+	    Frase frasee = new Frase();
+	    frasee.setContenuto(frase.trim());
+	    daoFrase.save(frasee);
 	}
+	
+	String[] words = text.split("\\s+");
+    for (String word : words) {
+    	Parole parola = new Parole();
+    	parola.setContenuto(word);
+    	daoParola.save(parola);
+    }
+        
+	
+	}
+	
+
+
 	
 	
 }
+
+
 
